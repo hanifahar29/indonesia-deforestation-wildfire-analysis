0a89a3d2a73ba37ced6f6935ba98fed8e2963e76
Hanifah Arrasyidah <86841245+hanifahar29@users.noreply.github.com>
2026-08-06 14:14:29 +0700

    feat: update query province deforestation analysis

--- query/province_deforestation.sql
+++ query/province_deforestation.sql
@@ -2,7 +2,7 @@
 SELECT * FROM province_deforestation
 ORDER BY province ASC
 
-/** Highest tree cover loss year **/
+/** Top 5 Province with the highest tree cover loss **/
 SELECT 
 	province,
 	SUM(tree_cover_loss_ha) AS total_tc_loss_ha
@@ -11,7 +11,7 @@ GROUP BY province
 ORDER BY SUM(tree_cover_loss_ha) DESC
 LIMIT 5;
 
-/** Lowest tree cover loss year **/
+/** Top 5 province with the lowest tree cover loss **/
 SELECT
 	province,
 	SUM(tree_cover_loss_ha) AS total_tc_loss_ha
@@ -20,5 +20,40 @@ GROUP BY province
 ORDER BY SUM(tree_cover_loss_ha) ASC
 LIMIT 5;
 
-/
-
+/** Trend tree cover loss per province **/
+SELECT
+	province,
+	year,
+	tree_cover_loss_ha,
+	LAG(tree_cover_loss_ha) OVER (
+		PARTITION BY province
+		ORDER BY year
+	) AS tc_last_year,
+	tree_cover_loss_ha - LAG(tree_cover_loss_ha) OVER (
+		PARTITION BY province
+		ORDER BY year
+	) AS change_ha
+FROM province_deforestation
+ORDER BY province, year;
+ 
+/** Province with the highest tree cover loss each year **/
+WITH province_rank AS(
+	SELECT
+		year,
+		province,
+		tree_cover_loss_ha,
+		ROW_NUMBER()OVER(
+			PARTITION BY year
+			ORDER BY tree_cover_loss_ha DESC
+		) AS pro_rank
+	FROM province_deforestation
+)
+SELECT
+	year,
+	province,
+	tree_cover_loss_ha,
+	pro_rank
+FROM province_rank
+WHERE pro_rank <= 3
+ORDER BY year;
+	
